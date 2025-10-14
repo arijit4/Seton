4e78153cbaf3ab5f6b6844faf620fbdc2b472370
arijit4 <akarijit4@gmail.com>
2025-10-14 13:30:30 +0600

    feat: Add InfoCard component for settings screen

--- app/src/main/java/com/arijit4/nothing/notes/ui/screen/shared/components/Scaffold.kt
+++ app/src/main/java/com/arijit4/nothing/notes/ui/screen/shared/components/Scaffold.kt
@@ -5,13 +5,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 
 @Composable
 fun CustomScaffold(
     modifier: Modifier = Modifier,
     title: String? = null,
+    navController: NavHostController? = null,
     topBar: @Composable () -> Unit = {
-        if (title != null) CustomTopBar(title = title)
+        if (title != null) CustomTopBar(title = title, navController = navController)
     },
     floatingActionButton: @Composable () -> Unit = {},
     content: @Composable (PaddingValues) -> Unit
